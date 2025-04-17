3230a9014826746d0a107adeffea7c99b20e6737
TFA05 <b2sinadinovic@gmail.com>
2025-04-17 23:14:00 +0200

    Do excercises 01-07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-};
+/*const removeFromArray = function(arr, ...badNumList) {
+    for (let badNum of badNumList){
+        for (let i = arr.length - 1; i >= 0; i--){
+            if (arr[i] === badNum)
+                arr.splice(i, 1);
+        }
+    }
+
+    return arr;
+};*/
+
+const removeFromArray = function(arr, ...badNumList){
+    return arr.filter((num) => !badNumList.includes(num))
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, lastNum) {
+    let sum = 0;
+    if (firstNum < 0 || lastNum < 0) return 'ERROR';
+    if (typeof firstNum != 'number' || typeof lastNum != 'number') return 'ERROR';
+    
+    if (Math.floor(firstNum) != firstNum || Math.floor(lastNum) != lastNum){
+        return 'ERROR';
+    }
+    if (firstNum > lastNum){
+        let change = firstNum;
+        firstNum = lastNum;
+        lastNum = change;
+    }
+    
+    for(let i = firstNum; i <= lastNum; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
